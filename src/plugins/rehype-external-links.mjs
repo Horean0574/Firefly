29e60175c0212a711cd7a60baf0d37f35e11de5e
Horean0574 <Horean0574@outlook.com>
2026-02-23 08:13:10 +0800

    updated

--- src/plugins/rehype-external-links.mjs
+++ src/plugins/rehype-external-links.mjs
@@ -37,7 +37,7 @@ export default function rehypeExternalLinks(options = {}) {
 			}
 
 			node.properties.target = "_blank";
-			node.properties.rel = "noopener";
+			node.properties.rel = "noopener noreferrer";
 		});
 	};
 }
