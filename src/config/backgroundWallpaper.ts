b1c97d4184c821b5e1ff72cd5b7efcbc5fb11593
Horean0574 <Horean0574@outlook.com>
2026-06-14 10:31:37 +0800

    updated

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -70,7 +70,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 是否允许用户通过控制面板切换横幅标题显示
-			switchable: true,
+			switchable: false,
 			// 主页横幅主标题
 			title: "拨云见日，洞见本真",
 			// 主页横幅主标题字体大小
