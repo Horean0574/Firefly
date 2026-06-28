35f7f45b7aaab0f867528b39832ee237566d0e8a
Horean0574 <Horean0574@outlook.com>
2026-06-28 11:33:34 +0800

    updated

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -42,7 +42,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件位置
 			position: "top",
 			// 是否在文章详情页显示
-			showOnPostPage: false,
+			showOnPostPage: true,
 		},
 		{
 			// 组件类型：公告组件
@@ -94,6 +94,18 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 				collapseThreshold: 10,
 			},
 		},
+		{
+			// 组件类型：侧边栏目录组件（只在文章详情页显示）
+			type: "sidebarToc",
+			// 是否启用该组件
+			enable: true,
+			// 组件位置
+			position: "sticky",
+			// 是否在文章详情页显示
+			showOnPostPage: true,
+			// 是否在非文章详情页显示
+			showOnNonPostPage: false,
+		},
 	],
 
 	// 右侧边栏组件配置列表
@@ -106,7 +118,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件位置
 			position: "top",
 			// 是否在文章详情页显示
-			showOnPostPage: true,
+			showOnPostPage: false,
 		},
 		{
 			// 组件类型：站点信息组件
@@ -144,7 +156,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 				},
 			},
 		},
-		{
+		/*{
 			// 组件类型：侧边栏目录组件（只在文章详情页显示）
 			type: "sidebarToc",
 			// 是否启用该组件
@@ -155,7 +167,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			showOnPostPage: true,
 			// 是否在非文章详情页显示
 			showOnNonPostPage: false,
-		},
+		},*/
 		{
 			// 组件类型：广告栏组件 1
 			type: "advertisement",
